1c6d56541c428b54676442033ae49dea700e1671
RahulRmCoder <rahulrajasekharanmenon64325@gmail.com>
2024-06-11 19:07:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+const port = 3000;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function passwordCheck(req, res, next) {
+    const password = req.body.password;
+    if (password === "MySecret@123") {
+        req.userIsAuthorised = true;
+    } else {
+        req.userIsAuthorised = false;
+    }
+    next();
+}
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+app.post("/check", passwordCheck, (req, res) => {
+    if (req.userIsAuthorised) {
+        res.sendFile(__dirname + "/public/secret.html");
+    } else {
+        res.sendFile(__dirname + "/public/index.html");
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
